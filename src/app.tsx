3ec955ac0a80e501971d67966f7fe3dfca096c6b
antonio-pas <antonio.b.passos18@gmail.com>
2023-11-18 12:57:30 -0800

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import icon from '/favicon.svg'
+import './app.css'
+
+export function App() {
+  return (
+    <>
+      <div>
+        <img src={icon} class="logo" alt="Vite logo" />
+      </div>
+    </>
+  )
+}
