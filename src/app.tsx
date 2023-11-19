9a99dc1d036018f0acce9ef1b52be78188961f5c
antonio-pas <antonio.b.passos18@gmail.com>
2023-11-19 10:07:57 -0800

    Added some data structures for exercises and schedules

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,46 @@
-import icon from '/favicon.svg'
 import './app.css'
+interface StrengthTrainingExercise {
+  type: "upper" | "lower" | "core" | "skills" | "recovery"
+}
+interface RunningExercise {
+  type: "sprint" | "tempo" | "base" | "long" | "recovery"
+  distance: number
+}
+type Exercise = {
+  type: StrengthTrainingExercise | RunningExercise
+  duration: number
+}
+interface AmPmSchedule {
+  am: Exercise
+  pm: Exercise
+}
+interface WholeDaySchedule {
+  wholeDay: Exercise
+}
+interface Time {
+  hour: number
+  minute: number
+}
+interface CustomSchedule {
+  exercises: Map<Time, Exercise>
+}
+type Schedule = AmPmSchedule | WholeDaySchedule | CustomSchedule;
+interface Day {
+  schedule: Schedule;
+}
 
 export function App() {
   return (
     <>
-      <div>
-        <img src={icon} class="logo" alt="Vite logo" />
+      <div class="container">
+        <div class="date">
+          {new Date().toLocaleDateString()}
+        </div>
+        <div class="day">
+          <h1>Title</h1>
+          <p>AM: Easy Run (2 miles)</p>
+          <p>PM: Upper Body Workout</p>
+        </div>
       </div>
     </>
   )
